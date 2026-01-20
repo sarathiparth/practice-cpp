d09c65b29c1eeabfa4175c189235dac716407f3a
sarathiparth <sarathigiriparth@gmail.com>
2026-01-20 13:09:32 +0530

    this program is to under stand the pointer using pointer

---
+++ arraypointer.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int size;
+    cin>>size;
+    int *p = new int[size];
+    
+    for(int i = 0;i<size;i++){
+        p[i] = i + 2 ;
+    }
+
+    for(int j = 0 ; j<size;j++){
+        cout<<p[j];
+    }
+
+    return 0;
+}
